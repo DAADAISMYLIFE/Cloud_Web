93ae49383f65944443f4ce3698788d996a7ce19f
cloud_qkekdhd <qkekdhd@naver.com>
2024-11-24 20:33:34 +0900

    서버 구동 확인

--- ROOT/WEB-INF/classes/cloud/DBConnection.java
+++ ROOT/WEB-INF/classes/cloud/DBConnection.java
@@ -6,7 +6,7 @@ import java.sql.DriverManager;
 public class DBConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/CLOUD";
     private static final String USER = "root";
-    private static final String PASSWORD = "tnseoaos123!"; // db 연결해야됨
+    private static final String PASSWORD = "1234";
     private static Connection connection = null;
 
     public static Connection getConnection() {
